91acac01dd9b6f592e68f0429600c839178bd77f
Lucas Vieira <59543597+lkasvr@users.noreply.github.com>
2023-11-04 16:56:59 -0300

    Simple offline HTML message

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,10 +1,21 @@
+var responseContent =
+  "<html>" +
+  "<body>" +
+  "<style>" +
+  "body {text-align: center; background-color: #333; color: #eee;}" +
+  "</style>" +
+  "<h1>Gotham Imperial Hotel</h1>" +
+  "<p>There seems to be a problem with your connections.</p>" +
+  "<p>Come visit us at Imperial Plaza, Gotham City for free WiFi.</p>" +
+  "</body>" +
+  "</html>";
+
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     fetch(event.request).catch(function () {
       return new Response(
-        "Welcome to the Gotham Imperial Hotel.\n" +
-        "There seems to be a problem with your connection.\n" +
-        "We look forward to telling you about our hotel as soon as you go online."
+        responseContent,
+        { headers: { "Content-Type": "text/html" } }
       );
     })
   );
