51e7f4738d97e66ba7ef11e6e43344a176450db6
Lucas Vieira <59543597+lkasvr@users.noreply.github.com>
2023-11-05 22:27:51 -0300

    Storing Requests in CacheStorage

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,22 +1,7 @@
-var responseContent =
-  "<html>" +
-  "<body>" +
-  "<style>" +
-  "body {text-align: center; background-color: #333; color: #eee;}" +
-  "</style>" +
-  "<h1>Gotham Imperial Hotel</h1>" +
-  "<p>There seems to be a problem with your connections.</p>" +
-  "<p>Come visit us at Imperial Plaza, Gotham City for free WiFi.</p>" +
-  "</body>" +
-  "</html>";
-
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    fetch(event.request).catch(function () {
-      return new Response(
-        responseContent,
-        { headers: { "Content-Type": "text/html" } }
-      );
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("gih-cache").then(function (cache) {
+      return cache.add("/index-offline.html");
     })
   );
 });
