370d77bc7861f92f33a9eecc16044569d8d67425
Lucas Vieira <59543597+lkasvr@users.noreply.github.com>
2023-11-05 23:27:13 -0300

    Gotham Imperial Hotel's branded offline page

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -7,18 +7,18 @@ var CACHED_URLS = [
   "/img/logo-header.png"
 ];
 
-self.addEventListener("install", function (event) {
+self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(CACHED_URLS);
     })
   );
 });
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", function(event) {
   event.respondWith(
-    fetch(event.request).catch(function () {
-      return caches.match(event.request).then(function (response) {
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
         if (response) {
           return response;
         } else if (event.request.headers.get("accept").includes("text/html")) {
