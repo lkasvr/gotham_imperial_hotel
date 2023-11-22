b9fd96afc45cd7599daa65aaae90a2f0d4e239c7
lkasvr <lkasvr@outlook.com>
2023-11-22 18:09:25 -0300

    Reusing Cached Response

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "gih-cache";
+var CACHE_NAME = "gih-cache-v3";
 var CACHED_URLS = [
   "/index-offline.html",
   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
@@ -7,10 +7,53 @@ var CACHED_URLS = [
   "/img/logo-header.png"
 ];
 
+var immutableRequests = [
+  "/fancy_header_background.mp4",
+  "/vendor/bootstrap/3.7.7/bootstrap.min.css",
+  "/css/style-v355.css"
+];
+var mutableRequests = [
+  "app-settings.json",
+  "index.html"
+];
+
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("gih-cache-v2").then(function (cache) {
+      var newImmutableRequests = [];
+      return Promise.all(
+        immutableRequests.map(function (url) {
+          return caches.match(url).then(function (response) {
+            if (response)
+              return cache.put(url, response);
+            
+            newImmutableRequests.push(url);
+            return Promise.resolve();
+          });
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (CACHE_NAME !== cacheName && cacheName.startsWith("gih-cache")) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
