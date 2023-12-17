de9565d8c6593449008cdb4799364f56efecfcca
lkasvr <lkasvr@outlook.com>
2023-12-17 12:22:32 -0300

    The account page's empty application shell

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -2,26 +2,32 @@ var CACHE_NAME = "gih-cache-v4";
 var CACHED_URLS = [
   // Our HTML
   "/index.html",
+  "/my-account.html",
   // Stylesheets
   "/css/gih.css",
   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
   "https://fonts.googleapis.com/css?family=Lato:300,600,900",
-  // Javascript
+  // JavaScript
   "https://code.jquery.com/jquery-3.0.0.min.js",
   "/js/app.js",
+  "/js/offline-map.js",
+  "/js/my-account.js",
   // Images
   "/img/logo.png",
   "/img/logo-header.png",
   "/img/event-calendar-link.jpg",
   "/img/switch.png",
   "/img/logo-top-background.png",
+  "/img/jumbo-background-sm.jpg",
   "/img/jumbo-background.jpg",
   "/img/reservation-gih.jpg",
   "/img/about-hotel-spa.jpg",
   "/img/about-hotel-luxury.jpg",
   "/img/event-default.jpg",
+  "/img/map-offline.jpg",
   // JSON
   "/events.json",
+  "/reservations.json"
 ];
 var googleMapsAPIJS = "https://maps.googleapis.com/maps/api/js?key="+
   "AIzaSyDm9jndhfbcWByQnrivoaWAEQA8jy3COdE&callback=initMap";
@@ -51,7 +57,24 @@ self.addEventListener("fetch", function (event) {
         });
       })
     );
-    // Handl requests for Google Maps JavaScript API file
+  } else if (requestURL.pathname === '/my-account') {
+    event.respondWith(
+      caches.match("/my-account.html").then(function (response) {
+        return response || fetch("/my-account.html");
+      })
+      );
+    } else if (requestURL.pathname === '/reservations.json') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        }).catch(function () {
+          return caches.match(event.request);
+        });
+      })
+    );
+      // Handl requests for Google Maps JavaScript API file
   } else if (requestURL.href === googleMapsAPIJS) {
     event.respondWith(
       fetch(
@@ -109,7 +132,7 @@ self.addEventListener("fetch", function (event) {
 self.addEventListener("activate", function(event) {
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
-      return Promise.all(
+      return Promise.all(~
         cacheNames.map(function (cacheName) {
           if (CACHE_NAME !== cacheName && cacheName.startsWith("gih-cache")) {
             return caches.delete(cacheName);
